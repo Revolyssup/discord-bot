0477028e66f44a809f2b7916009e5a13e0f57fa6
Ashish <ashishjaitiwari15112000@gmail.com>
2021-01-22 20:25:14 +0530

    ignored stuff

--- dist/handler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function handlebot(msg) {
-    console.log(`${msg.author.tag}: ${msg.content}`);
-    if (msg.content === 'bruh') {
-        msg.reply("Hy! Im ashish's bot");
-    }
-}
-exports.default = handlebot;
